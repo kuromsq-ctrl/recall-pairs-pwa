8012debe2f369571e164b0e370b29442a29a2a93
kuromsq-ctrl <kuro.msq@gmail.com>
2026-01-02 16:57:34 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE="recall-pairs-pwa-v8";
+const ASSETS=[
+  "./","./index.html","./styles.css","./app.js","./manifest.webmanifest",
+  "./icon-192.png","./icon-512.png",
+  "./words_low.txt","./words_mid.txt","./words_high.txt","./words_abs.txt"
+];
+self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>(k===CACHE)?null:caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
